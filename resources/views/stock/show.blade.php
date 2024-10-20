ae873487e411668ce96331f8f6db6fa95dfa8514
daniel <danielcruzram123@gmail.com>
2024-10-20 17:45:50 -0600

    sku

--- resources/views/stock/show.blade.php
+++ resources/views/stock/show.blade.php
@@ -19,7 +19,7 @@
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" class="px-6 py-3">
-                            Id
+                            Sku
                         </th>
                         <th scope="col" class="px-6 py-3">
                             Nombre
@@ -52,7 +52,7 @@
                         @foreach ($stock as $stocks)
                             <th scope="row"
                                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {{ $stocks->materiale_id }}
+                                {{ $stocks->materiale->sku }}
                             </th>
                             <th scope="row"
                                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
